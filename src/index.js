6c8bffa74ecf04decaa9667ed2549d227f5375fd
mrsamdev <sijosam1905@gmail.com>
2021-11-05 20:42:43 +0530

    redux-saga-mui added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
+import store from "./store/configure-store";
+import { Provider } from "react-redux";
+
+const HigherOrderComponentsWrappeedApp = () => (
+	<ThemeProvider theme={theme}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</ThemeProvider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<HigherOrderComponentsWrappeedApp />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
