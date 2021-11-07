971a89975241b1add7cac43a8feec9504276bac7
mrsamdev <sijosam1905@gmail.com>
2021-11-07 12:33:59 +0530

    graph added

--- src/pages/graph/index.js
+++ src/pages/graph/index.js
@@ -1,13 +1,55 @@
 import Page from "../../components/shared/page";
+import { Scatter } from "react-chartjs-2";
 export default function Graph() {
 	return (
 		<Page title="Graph">
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-				Learn React
-			</a>
+			<ScatterChart />
 		</Page>
 	);
 }
+
+const rand = () => Math.round(Math.random() * 20 - 10);
+
+const data = {
+	datasets: [
+		{
+			label: "A dataset",
+			data: [
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+				{ x: rand(), y: rand() },
+			],
+			backgroundColor: "rgba(255, 99, 132, 1)",
+		},
+	],
+};
+
+const ScatterChart = () => (
+	<>
+		<Scatter
+			data={data}
+			options={{
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								beginAtZero: true,
+							},
+						},
+					],
+				},
+			}}
+		/>
+	</>
+);
